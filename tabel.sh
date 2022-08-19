1b90c0c2b0b11b07712acc82f81e870d629b491c
Raif Coonjah <raifcoonjah@pm.me>
2022-08-19 17:33:57 +0400

    feat: add arch install

--- tabel.sh
+++ tabel.sh
@@ -20,6 +20,7 @@ ${ENDCOLOR}
 "
 
 echo "Please choose your distro: "
+echo "When using tabel we expect you already have a working install of your selected distro. "
 
 
 supported_distros=("Arch Linux" "Debian/Ubuntu" "Fedora" "Quit/Exit")
@@ -29,14 +30,15 @@ do
         "Arch Linux")
             echo -e "${BLUE}Running Arch basic setup..${ENDCOLOR}"
 
-
             ## Basic tools that I think should be installed by default on any machine. 
-            echo -e "${BLUE}:: Installing Git, curl, wget & Zsh..${ENDCOLOR}"
-            sudo pacman -S git curl wget zsh --noconfirm
+            echo -e "${BLUE}:: Installing Git, curl, nano, wget & Zsh..${ENDCOLOR}"
+            sudo pacman -S --noconfirm git curl wget zsh nano 
 
+            echo -e "${BLUE}:: Installing tar, gzip, bzip2, unzip, unrar & p7zip.. ${ENDCOLOR}"
+            pacman -S --noconfirm tar gzip bzip2 unzip unrar p7zip
             ## Flatpak support
             echo -e "${BLUE}Installing and adding flatpak support..${ENDCOLOR}"
-            sudo pacman -S flatpak --noconfirm
+            sudo pacman -S --noconfirm flatpak 
 
             ## Installing yay-bin 
             echo -e "${BLUE}:: Installing Yay (AUR Helper)${ENDCOLOR}"
@@ -54,8 +56,14 @@ do
             echo "Building package.."
             makepkg -si
 
-            ## Installing Nerd fonts
+            ## Installing fonts
 
+            echo -e "${GREEN}:: Installing fonts...${ENDCOLOR}"
+
+            sudo pacman --noconfirm -S noto-fonts-cjk noto-fonts-extra noto-fonts-emoji ttf-dejavu
+
+            echo -e "${GREEN}:: Basic Arch setup: Done. ${ENDCOLOR}"
+            echo " To install gaming packages please choose ArchGaming.."
 
             ;;
         "Debian/Ubuntu")
