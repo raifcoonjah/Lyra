3bef2d606c06b1f645f33b9ac6489a0c7e39aa65
Raif Coonjah <raifcoonjah@pm.me>
2022-10-09 12:08:12 +0400

    feat: add experimental wine depencies to arch setup
    * Proper testing is still needed.

--- tabel.sh
+++ tabel.sh
@@ -58,13 +58,30 @@ do
 
             ## Installing fonts
 
+            echo -e "${GREEN}:: Installing wine depencies....${ENDCOLOR}"
+            echo -e "${GREEN}:: We use the Lutris wine depencies list ${ENDCOLOR}"
+            echo -e "${GREEN}:: Source here: https://github.com/lutris/docs/blob/master/WineDependencies.md${ENDCOLOR}"
+
+            echo -e "${RED}::Enabling multilib (TESTING REQUIRED..)${ENDCOLOR}"
+
+            sed -i 's/#\[multilib]/\[multilib]/g' /etc/pacman.conf
+
+            sudo pacman -Syu --noconfirm
+
+            sudo pacman -S --needed wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls \
+            mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error \
+            lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo \
+            sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama \
+            ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 \
+            lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
+
+            echo -e "${GREEN}:: Wine/Lutris depencies installed! ${ENDCOLOR}"
+
             echo -e "${GREEN}:: Installing fonts...${ENDCOLOR}"
 
             sudo pacman --noconfirm -S noto-fonts-cjk noto-fonts-extra noto-fonts-emoji ttf-dejavu
 
             echo -e "${GREEN}:: Basic Arch setup: SUCCESSFUL! ${ENDCOLOR}"
-            # echo " To install gaming packages please choose ArchGaming.."
-
             ;;
         "Debian/Ubuntu")
             echo "Selected Debian/Ubuntu.."
